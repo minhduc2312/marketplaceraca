fd8e4652ca35d0c671f1592eda8302511e904fb3
Sercret231298 <minhduc231298@gmail.com>
2022-05-03 13:42:44 +0700

    install package backend

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
+console.log(process.env.MONGODB_CONNECTION_STRING)
 const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
