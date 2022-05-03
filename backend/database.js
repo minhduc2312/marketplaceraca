b0f2464aeebbcc0dc3aa0d6012e94d7f99ec4c90
Sercret231298 <minhduc231298@gmail.com>
2022-05-03 19:49:39 +0700

    fix config:

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const dotenv = require('dotenv');
-dotenv.config();
-console.log(process.env.MONGODB_CONNECTION_STRING)
-const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const { MONGODB_CONNECTION } = require('../config')
+const client = new MongoClient(MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
